24529910e087c509da6339aa2672b66542517e17
vijayanandraj <vijayanandraj@gmail.com>
2025-07-21 10:55:24 +0530

    Add regex patterns for sensitive data detection in HelloController

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -2,6 +2,16 @@
 using Microsoft.AspNetCore.Mvc;
 namespace HwRestApp.Controllers;
 
+PATTERNS = [
+    re.compile(r'\b\d{3}-?\d{2}-?\d{4}\b'),              
+    re.compile(r'\b\d{12}\b'),                           
+    re.compile(r'\b0[A-Za-z0-9_-]{63}\b'),               
+    re.compile(r'\bAKC[p]?[A-Za-z0-9_-]{40}\b'),         
+    re.compile(r'\beyJ[0-9A-Za-z._-]{10,}\.[0-9A-Za-z._-]{10,}\.[0-9A-Za-z._-]{10,}\b'),  
+    re.compile(r'(?:password|passwd|pwd)[\s:=]+.*', re.I),
+    re.compile(r'\b[nz](?![aeiou])[a-z0-9]{6}\b', re.I),
+]
+
 
 [ApiController]
 [Route("api/hello")]
